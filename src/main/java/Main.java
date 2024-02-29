318a01f137da967feb9916ec0ba12f95e4e7a156
Danny Cao <dannycao1997@yahoo.com>
2024-02-29 16:29:36 -0500

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
@@ -5,12 +7,27 @@
 public class Main {
 
     public static void main(String[] args ){
+        Main main = new Main();
+        main.compute();
+
         // create a new object of class Main
 
         // call the compute() method on that new object
     }
 
     public void compute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("You are neither Alice nor Bob,");
+
+        }
+
+
         // Get input string of name from user
         
         // Create conditional to check name against Alice and Bob
